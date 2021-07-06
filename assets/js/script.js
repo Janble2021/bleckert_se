fcaa0ef140ef74e0d43cb4d7549c0c34cadd4c4e
Danster2020 <daniel.bleckert@me.com>
2021-07-06 14:36:00 +0200

    cookies

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,17 +17,37 @@ if (document.getElementsByClassName("mySlides").length != 0) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}    
-    if (n < 1) {slideIndex = slides.length}
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";  
+      slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+      dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
   }
 
-  setInterval(function(){ plusSlides(1); }, 5000);
+  setInterval(function () { plusSlides(1); }, 5000);
+}
+
+
+function cookieBanner() {
+  var banner = document.getElementById("cookie_banner");
+  var consent = localStorage.getItem("cookie_consent");
+  if (consent == null) {
+    banner.style.display = "flex";
+  } else {
+    banner.style.display = "none";
+  }
+}
+
+function allowCookies(choice) {
+  if (choice == true) {
+    localStorage.setItem("cookie_consent", true);
+  } else {
+    localStorage.setItem("cookie_consent", false);
+  }
+  cookieBanner()
 }
